b331eaee4ec291f0b814d2c1ae0f4404acaba52b
voice4net <brad@voice4net.com>
2018-02-27 16:32:17 -0600

    formatted

--- voice4net_add_gateway.pl
+++ voice4net_add_gateway.pl
@@ -9,7 +9,7 @@ use Getopt::Long;
 
 my $gateway_name;
 my $proxy;
-my $register;
+my $register="true";
 my $caller_id_in_from;
 my $from_user;
 my $from_domain;
@@ -23,13 +23,13 @@ my $config_path;
 GetOptions(
    'gateway-name=s'      		=> \$gateway_name,
    'proxy=s'             		=> \$proxy,
-   'register=s'          		=> \$register,     
-   'caller-id-in-from=s'   		=> \$caller_id_in_from,        
+   'register=s'          		=> \$register,
+   'caller-id-in-from=s'   		=> \$caller_id_in_from,
    'from-user=s'         		=> \$from_user,
    'from-domain=s'       		=> \$from_domain,
    'extension=s'         		=> \$extension,
-   'extension-in-contact=s'     => \$extension_in_contact,   
-   'sip-contact-user=s'		    => \$sip_contact_user,   
+   'extension-in-contact=s'     => \$extension_in_contact,
+   'sip-contact-user=s'		    => \$sip_contact_user,
    'username=s'          		=> \$username,
    'password=s'      	 		=> \$password,
    'confpath=s'          		=> \$config_path
@@ -44,47 +44,47 @@ if (! $config_path)
 	$config_path = "/etc/freeswitch/";
 }
 
-my $gateway_template = "<include>\n";
+my $gateway_config = "<include>\n";
 
-$gateway_template .= "\t<gateway name=\"" . $gateway_name . "\">\n";
-$gateway_template .= "\t  <param name=\"proxy\" value=\"" . $proxy . "\"/>\n";
+$gateway_config .= "\t<gateway name=\"" . $gateway_name . "\">\n";
+$gateway_config .= "\t\t<param name=\"proxy\" value=\"" . $proxy . "\"/>\n";
 
 if ($register)
 {
-	$gateway_template .= "\t  <param name=\"register\" value=\"" . $register . "\"/>\n";
+	$gateway_config .= "\t\t<param name=\"register\" value=\"" . $register . "\"/>\n";
 }
 
 if ($caller_id_in_from)
 {
-	$gateway_template .= "\t  <param name=\"caller-id-in-from\" value=\"" . $caller_id_in_from . "\"/>\n";
+	$gateway_config .= "\t\t<param name=\"caller-id-in-from\" value=\"" . $caller_id_in_from . "\"/>\n";
 }
 
-$gateway_template .= "\t  <param name=\"from-user\" value=\"" . $from_user . "\"/>\n";
+$gateway_config .= "\t\t<param name=\"from-user\" value=\"" . $from_user . "\"/>\n";
 
 if ($from_domain)
 {
-	$gateway_template .= "\t  <param name=\"from-domain\" value=\"" . $from_domain . "\"/>\n";
+	$gateway_config .= "\t\t<param name=\"from-domain\" value=\"" . $from_domain . "\"/>\n";
 }
 
 if ($extension)
 {
-	$gateway_template .= "\t  <param name=\"extension\" value=\"" . $extension . "\"/>\n";
+	$gateway_config .= "\t\t<param name=\"extension\" value=\"" . $extension . "\"/>\n";
 }
 
 if ($extension_in_contact)
 {
-	$gateway_template .= "\t  <param name=\"extension-in-contact\" value=\"" . $extension_in_contact . "\"/>\n";
+	$gateway_config .= "\t\t<param name=\"extension-in-contact\" value=\"" . $extension_in_contact . "\"/>\n";
 }
 
 if ($sip_contact_user)
 {
-	$gateway_template .= "\t  <param name=\"sip-contact-user\" value=\"" . $sip_contact_user . "\"/>\n";
+	$gateway_config .= "\t\t<param name=\"sip-contact-user\" value=\"" . $sip_contact_user . "\"/>\n";
 }
 
-$gateway_template .= "\t  <param name=\"username\" value=\"" . $username . "\"/>\n";
-$gateway_template .= "\t  <param name=\"password\" value=\"" . $password . "\"/>\n";
+$gateway_config .= "\t\t<param name=\"username\" value=\"" . $username . "\"/>\n";
+$gateway_config .= "\t\t<param name=\"password\" value=\"" . $password . "\"/>\n";
 
-$gateway_template .= "\t</gateway>\n</include>";
+$gateway_config .= "\t</gateway>\n</include>";
 
 my $new_gateway_file_name = $config_path . '/sip_profiles/external/' . $gateway_name . '.xml';
 
@@ -95,7 +95,7 @@ if ( ! $fh ) {
 	return -1;
 }
 
-print $fh $gateway_template;
+print $fh $gateway_config;
 close($fh);
 
 system("chown freeswitch:freeswitch ".$new_gateway_file_name);
